df547bb9494c74076bd7dd6df29f5c094b9eab45
BuKwon Kim <dnu05043@naver.com>
2024-02-27 21:52:37 +0900

    section 3-8

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -1,10 +1,9 @@
-package hello.core;
+package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.order.Order;
-import hello.core.order.OrderService;
 
 public class OrderApp {
 
@@ -17,7 +16,7 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
         System.out.println("order.caculatePrice() = " + order.caculatePrice());
